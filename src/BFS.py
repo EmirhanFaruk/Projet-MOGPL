ceb751e6840a9310823c24a600993917b475ed2f
EmirhanFaruk <emirhansagir74@gmail.com>
2025-12-07 00:42:27 +0100

    changing bfs to use tuples

--- src/BFS.py
+++ src/BFS.py
@@ -1,8 +1,3 @@
-import numpy as np
-
-from State import State
-
-
 advance_action = {"a1": 1, "a2": 2, "a3": 3}
 advance_directions = {
     0: (0, -1),  # North
@@ -37,21 +32,21 @@ def advance(state, action, grid):
 
     can_advance = True # Assume True unless proven otherwise
     for i in range(1, advance_num):
-        x = state.x + (i * advance_directions[state.o][0])
-        y = state.y + (i * advance_directions[state.o][1])
+        x = state[0] + (i * advance_directions[state[2]][0])
+        y = state[1] + (i * advance_directions[state[2]][1])
         can_advance = can_advance and valid_position(x, y, grid)
     
     if can_advance:
-        new_x = state.x + (advance_num * advance_directions[state.o][0])
-        new_y = state.y + (advance_num * advance_directions[state.o][1])
-        return State(new_x, new_y, state.o, state, action)
+        new_x = state[0] + (advance_num * advance_directions[state[2]][0])
+        new_y = state[1] + (advance_num * advance_directions[state[2]][1])
+        return (new_x, new_y, state[2])
 
 def turn(state, action):
     """
     Turns the state left or right based on the action. Returns the new state.
     """
-    new_o = (state.o + (1 if action == "D" else -1)) % 4
-    return State(state.x, state.y, new_o, state, action)
+    new_o = (state[2] + (1 if action == "D" else -1)) % 4
+    return (state[0], state[1], new_o)
 
 def perform_action(current_state, action, grid):
     """
@@ -63,15 +58,14 @@ def perform_action(current_state, action, grid):
         return advance(current_state, action, grid)
     return None
 
-def BFS(grid, start_pos, goal_pos):
+def BFS(grid, start_state, goal_state):
     """
     Performs a breadth-first search on the grid to find a path from the start position and orientation to the goal position.
     Returns a list of actions to reach the goal, or None if no path is found.
     """
-    start_x, start_y, start_o = start_pos
-    goal_x, goal_y = goal_pos
-    start_state = State(start_x, start_y, start_o)
+    start_x, start_y, start_o = start_state
     queue = [start_state]
+    parents = {(start_x, start_y, start_o): None}
     visited = set()
     visited.add((start_x, start_y, start_o))
 
@@ -79,19 +73,21 @@ def BFS(grid, start_pos, goal_pos):
         current_state = queue.pop(0)
 
         # Check if we reached the goal
-        if current_state.x == goal_x and current_state.y == goal_y:
+        if current_state[:2] == goal_state:
             path = [] # Reconstruct path
-            while current_state.parent is not None:
-                path.append(current_state.action)
-                current_state = current_state.parent
+            last_state = current_state
+            while parents[last_state] is not None:
+                last_state, last_action = parents[last_state]
+                path.append(last_action)
             return path[::-1]  # Return reversed path
 
         # Generate possible actions (move forward(1, 2 or 3), turn left, turn right)
         for action in ["a1", "a2", "a3", "D", "G"]:
             new_state = perform_action(current_state, action, grid)
             # If the new state is valid and not visited, add it to the queue
-            if new_state and (new_state.x, new_state.y, new_state.o) not in visited:
-                visited.add((new_state.x, new_state.y, new_state.o))
+            if new_state and (new_state[0], new_state[1], new_state[2]) not in visited:
+                visited.add((new_state[0], new_state[1], new_state[2]))
+                parents[new_state] = (current_state, action)
                 queue.append(new_state)
 
     return None  # No path found
